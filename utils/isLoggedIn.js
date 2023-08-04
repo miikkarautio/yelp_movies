c2fcee3ca5ced86d92415c3223642cd98cac17bc
Miikka Rautio <mikka.rautio01@gmail.com>
2023-08-04 10:11:13 +0000

    Add flash error message

--- utils/isLoggedIn.js
+++ utils/isLoggedIn.js
@@ -4,6 +4,7 @@ const isLoggedIn = (req, res, next) => {
 	if(req.isAuthenticated()){
 		return next();
 	} else{
+		req.flash("error", "Hey! You must be logged in");
 		res.redirect("/login");
 	}
 };
