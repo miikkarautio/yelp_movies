7f7c9e15d46090590f514e379487f8ba97481896
Miikka Rautio <mikka.rautio01@gmail.com>
2023-08-04 10:10:46 +0000

    Add flash messages to app.js

--- app.js
+++ app.js
@@ -66,9 +66,11 @@ app.use(morgan("tiny"));
 // const seed = require("./utils/seed");
 // seed();
 
-//Current user middleware config
+//Current user state config
 app.use((req, res, next) => {
 	res.locals.user = req.user;
+	res.locals.errorMessage = req.flash("error");
+	res.locals.successMessage = req.flash("success");
 	next();
 })
 
